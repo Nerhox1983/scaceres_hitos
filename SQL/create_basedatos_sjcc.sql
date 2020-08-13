9255400235732570cbc7452c0a61a120d6604d22
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-13 15:16:48 -0500

    Crear basedatos sjcc

---
+++ SQL/create_basedatos_sjcc.sql
@@ -0,0 +1,8 @@
+/*Autor:        Sergio J. Cáceres C. - scaceres@intergrupo.com.*/
+/*Fecha:        2020/08/13.*/
+/*Descripción:  Creación de base de datos: SJCC.*/
+
+USE MASTER
+GO
+
+CREATE DATABASE SJCC
