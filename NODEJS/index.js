c85a329bea022bb584cbd15b71bbba2d3122bbcc
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-13 17:44:42 -0500

    Agregar dbConfig y getTipos en index

---
+++ NODEJS/index.js
@@ -0,0 +1,39 @@
+var sql = require ('mssql');
+
+var dbConfig =
+{
+    server: 'localhost\\MSSQLSERVER2017'
+    , database: 'SJCC'
+    , user: 'sa'
+    , password: 'Intergrupo2017.*'
+    , port: 52684
+}
+
+function getTipos()
+{
+    var conn = new sql.ConnectionPool(dbConfig);
+    var req = new sql.Request(conn);
+    
+    conn.connect (function (err) 
+    {
+        if (err) 
+        {
+            console.log(err);
+            return;     
+        }
+        req.query("SELECT * FROM tipos", function(err, recordset) 
+        {
+            if (err) 
+            {
+                console.log(err);    
+            }
+            else 
+            {
+                console.log(recordset)
+            }
+            conn.close();
+        });
+    });
+}
+
+getTipos();
