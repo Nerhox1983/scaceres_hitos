9e2583619af516cd6704e79d5c2ef24eca6377b4
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-13 20:10:00 -0500

    Usar app.use en index.js

--- NODEJS/index.js
+++ NODEJS/index.js
@@ -1,9 +1,11 @@
-var express = require ('express');
-var bodyParser = require("body-parser");
-var app = express();
-app.use(function (req, res, next) {  
-    res.header("Access-Control-Allow-Origin", "*");  
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');  
-    next();  
-    }) 
+var express = require('express');  
+var bodyParser = require("body-parser");  
+var app = express();  
+app.use(function (req, res, next) 
+{  
+res.header("Access-Control-Allow-Origin", "*");  
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');  
+next();  
+});  
+app.use(bodyParser.urlencoded(extended, true ));
