42957e67b1d609307914fecf0ede03bcc5d3dfa0
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-18 16:11:21 -0500

    Ajustar propiedades de interface: hito.ts

---
+++ angular-hitos/src/app/interfaces/hito.ts
@@ -0,0 +1,11 @@
+export interface Hito 
+{    
+    id:	Number;
+    nombreProyecto:	string;
+    fechaIngreso: Date;
+    tipo: Number;
+    descripcion: string;
+    fechaCumplimiento: Date;
+    cumplio:	string;
+    observacionesCumplimiento: string;
+}
