88ac0e9187f13e28967ac3239c199dbdfd5907df
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 15:10:16 -0500

    Ajustar archivos basicos.

--- hitos/src/app/hitos/hitos.component.ts
+++ hitos/src/app/hitos/hitos.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Hito } from '../hito';
 
 @Component({
   selector: 'app-hitos',
   templateUrl: './hitos.component.html',
   styleUrls: ['./hitos.component.css']
 })
-export class HitosComponent implements OnInit {
+export class HitosComponent implements OnInit 
+{
+  hito: Hito = 
+  {
+    i_hitid: 1,
+    s_nompro: 'Windstorm',
+    f_fecing: new Date(),
+    i_tipo: 11,
+    s_descri: 'ABC',
+    f_feccum: new Date(),
+    s_cumpli: "N",
+    s_obscum: "BCD"
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() 
+  {
   }
 
+  ngOnInit() 
+  {
+  }
 }
