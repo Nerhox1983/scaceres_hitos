9f519911eeec858a9d21ad8a1f607a222e7653a8
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 16:55:13 -0500

    Ajustar hito-detail.component.ts

--- hitos/src/app/hito-detail/hito-detail.component.ts
+++ hitos/src/app/hito-detail/hito-detail.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hito } from '../hito';
 
 @Component({
-  selector: 'app-hito-detail',
+  selector: 'app-hitp-detail',
   templateUrl: './hito-detail.component.html',
   styleUrls: ['./hito-detail.component.css']
 })
-export class HitoDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hito;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() 
+  {
   }
 
+  ngOnInit() 
+  {
+  }
 }
