67b2e6f53bc94cdfcdb7d1834f2f3380ddaaafbc
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 14:29:50 -0500

    Crear component hitos en carpeta hitos y ajustar app.module.ts.

--- hitos/src/app/app.module.ts
+++ hitos/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HitosComponent } from './hitos/hitos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HitosComponent
   ],
   imports: [
     BrowserModule
---
+++ hitos/src/app/hitos/hitos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hitos',
+  templateUrl: './hitos.component.html',
+  styleUrls: ['./hitos.component.css']
+})
+export class HitosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
