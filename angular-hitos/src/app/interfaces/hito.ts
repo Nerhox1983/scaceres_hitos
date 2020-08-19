cc2d86626ac94ff116f3dc9ee4668a118454bb1c
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 12:12:25 -0500

    Ajustar hito.ts

--- angular-hitos/src/app/interfaces/hito.ts
+++ angular-hitos/src/app/interfaces/hito.ts
@@ -1,11 +1,11 @@
 export interface Hito 
 {    
-    id:	Number;
-    nombreProyecto:	string;
-    fechaIngreso: Date;
-    tipo: Number;
-    descripcion: string;
-    fechaCumplimiento: Date;
-    cumplio:	string;
-    observacionesCumplimiento: string;
+    i_hitid:	Number;
+    s_nompro:	string;
+    f_fecing: Date;
+    i_tipo: Number;
+    s_descri: string;
+    f_feccum: Date;
+    s_cumpli:	string;
+    s_obscum: string;
 }
