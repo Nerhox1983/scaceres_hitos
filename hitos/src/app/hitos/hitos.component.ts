77a8b18282214d7a4eed394b891ab6bf8ace65c2
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 16:17:25 -0500

    actualizar hitos.component.ts

--- hitos/src/app/hitos/hitos.component.ts
+++ hitos/src/app/hitos/hitos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hito } from '../hito';
+import { HITOS} from '../mock-hitos';
 
 @Component({
   selector: 'app-hitos',
@@ -8,23 +9,18 @@ import { Hito } from '../hito';
 })
 export class HitosComponent implements OnInit 
 {
-  hito: Hito = 
-  {
-    i_hitid: 1,
-    s_nompro: 'Z8',
-    f_fecing: new Date(),
-    i_tipo: 11,
-    s_descri: 'ABC',
-    f_feccum: new Date(),
-    s_cumpli: "N",
-    s_obscum: "BCD"
-  };
-
+  hitos = HITOS;
+  selectedHito: Hito;
   constructor() 
   {
   }
 
   ngOnInit() 
+  {    
+  }
+
+  onSelect (hito: Hito): void 
   {
+    this.selectedHito = hito;
   }
 }
