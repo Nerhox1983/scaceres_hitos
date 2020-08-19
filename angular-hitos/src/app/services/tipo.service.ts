c730b003eab505a72d8ef4209adefc89a518d70e
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-18 21:25:30 -0500

    Ajustar app.component.ts y tipo.service.ts

--- angular-hitos/src/app/services/tipo.service.ts
+++ angular-hitos/src/app/services/tipo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Tipo } from './../interfaces/tipo';
+import { Tipo } from './../interfaces/Tipo';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,11 @@ export class TipoService
 {
   constructor(private http: HttpClient)
   {
-
+  }
+  getTodosTipos()
+  {    
+    const path = 'http://localhost:5000/ApiGetTipos';
+      return this.http.get<Tipo[]>(path);
   }  
-}
+}  
+
