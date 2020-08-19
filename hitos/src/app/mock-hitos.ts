171a7fa14f583211581f80e8316e75200e575b21
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 16:07:34 -0500

    crear mock-hito.ts en app.

---
+++ hitos/src/app/mock-hitos.ts
@@ -0,0 +1,10 @@
+import { Hito } from './hito';
+
+export const HITOS: Hito[] = 
+[
+    {i_hitid:1, s_nompro:'A500', f_fecing: new Date(), i_tipo:1,s_descri:'test_02', f_feccum: new Date(), s_cumpli:'S', s_obscum:'prueba 01'},
+    {i_hitid:8,s_nompro:'A1', f_fecing:new Date(), i_tipo:2, s_descri:'test_16', f_feccum: new Date(), s_cumpli:'S', s_obscum:'prueba 06'},
+    {i_hitid:9,s_nompro:'T29', f_fecing: new Date(), i_tipo:3,s_descri:'test_11',f_feccum: new Date(), s_cumpli:'N', s_obscum:'prueba 75'},
+    {i_hitid:13,s_nompro:'Z4C', f_fecing: new Date(), i_tipo:8,s_descri:'test_74',f_feccum: new Date(), s_cumpli:'N', s_obscum:'prueba 29'},
+    {i_hitid:14,s_nompro:'J46', f_fecing: new Date(), i_tipo:1,s_descri:'test_39', f_feccum: new Date(), s_cumpli:'S', s_obscum:'prueba 02'}    
+];
