9a7905990481f0c9c9c030aa0130fc6fb2190e9a
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-19 08:42:31 -0500

    crear create_procedure_buscarHito.sql

---
+++ SQL/create_procedure_buscarHito.sql
@@ -0,0 +1,12 @@
+/*Autor:        Sergio J. Cáceres C. - scaceres@intergrupo.com.*/
+/*Fecha:        2020/08/19.*/
+/*Descripción:  Creación de procedimiento almacenado que busca un hito.*/
+USE [SJCC]
+GO
+
+CREATE PROCEDURE [dbo].[buscarHito]
+	@i_hitoId INT
+AS
+SELECT * FROM [dbo].[hitos]
+      WHERE i_hitid= @i_hitoId
+GO
