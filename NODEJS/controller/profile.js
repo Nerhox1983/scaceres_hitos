c9d36f94058bd24f478a9d5191c3a0969ccff22e
Sergio José Cáceres Cifuentes <Scaceres@intergrupo.com>
2020-08-18 21:12:47 -0500

    Ajustar app.component.css app.component.html y profile.js

--- NODEJS/controller/profile.js
+++ NODEJS/controller/profile.js
@@ -28,6 +28,32 @@ router.get('/ApiProfileGet', async (req, res) =>
     }  
 })
 
+router.get('/ApiGetHitos', async (req, res) => 
+{  
+    try 
+    {  
+        const pool = await poolPromise  
+        const result = await pool.request()  
+        .query('select * from tipos',function(err, profileset)
+        {  
+            if (err)  
+            {  
+                console.log(err)  
+            }  
+            else 
+            {  
+                var send_data = profileset.recordset;  
+                res.json(send_data);  
+            }  
+        })  
+    } 
+    catch (err) 
+    {  
+        res.status(500)  
+        res.send(err.message)  
+    }  
+})
+
 /**/
 router.post('/ApiProfilePost', async (req, res) => 
 {  
